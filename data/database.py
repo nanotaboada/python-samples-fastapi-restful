e3430e4d60ad8212c5d1f8c6a03176ab593fdea8
Nano Taboada <nanotaboada@users.noreply.github.com>
2023-11-06 14:47:15 -0300

    Upgraded FastAPI with dependencies from 0.68.0 to 0.104.1

--- data/database.py
+++ data/database.py
@@ -1,8 +1,7 @@
 # database.py
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 engine = create_engine("sqlite:///./data/songs.db", connect_args={"check_same_thread": False})
 OrmSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
