e9cee354975631df039448c0842c50715095f029
Nano Taboada <nanotaboada@users.noreply.github.com>
2024-04-07 18:03:53 -0300

    feat(api)!: implement POST, PUT and DELETE

--- routes/player_route.py
+++ routes/player_route.py
@@ -1,9 +1,9 @@
-# -------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Route
-# -------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 from typing import List
-from fastapi import APIRouter, Depends, HTTPException, status, Path
+from fastapi import APIRouter, Body, Depends, HTTPException, status, Path
 from data.player_database import OrmSession
 from sqlalchemy.orm import Session
 from models.player_model import PlayerModel
@@ -20,48 +20,126 @@ def get_orm_session():
     finally:
         orm_session.close()
 
-# -------------------------------------------------------------------------------------------------
-# HTTP GET
-# -------------------------------------------------------------------------------------------------
+# POST -------------------------------------------------------------------------
+
+
+@api_router.post(
+    "/players/",
+    status_code=status.HTTP_201_CREATED,
+    summary="Creates a new Player",
+)
+def create_player(
+    player_model: PlayerModel = None,
+    orm_session: Session = Depends(get_orm_session),
+):
+    if player_model is None:
+        player_model = Body(...)
+
+    if not player_model:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+    player = player_service.retrieve_by_id(orm_session, player_model.id)
+
+    if player:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT)
+
+    player_service.create(orm_session, player_model)
+
+# GET --------------------------------------------------------------------------
 
 
 @api_router.get(
     "/players/",
     response_model=List[PlayerModel],
+    status_code=status.HTTP_200_OK,
     summary="Retrieves a collection of Players"
 )
 def get_players(
     orm_session: Session = Depends(get_orm_session)
 ):
-    players = player_service.retrieve_all_players(orm_session)
+    players = player_service.retrieve_all(orm_session)
+
     return players
 
 
 @api_router.get(
     "/players/{player_id}",
     response_model=PlayerModel,
-    summary="Retrieves a Player by Id"
+    status_code=status.HTTP_200_OK,
+    summary="Retrieves a Player by its Id"
 )
 def get_player_by_id(
     player_id: int = Path(..., title="The Id of the Player"),
     orm_session: Session = Depends(get_orm_session)
 ):
-    player = player_service.retrieve_player_by_id(orm_session, player_id)
+    player = player_service.retrieve_by_id(orm_session, player_id)
+
     if not player:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
     return player
 
 
 @api_router.get(
     "/players/squadnumber/{squad_number}",
     response_model=PlayerModel,
-    summary="Retrieves a Player by Squad Number"
+    status_code=status.HTTP_200_OK,
+    summary="Retrieves a Player by its Squad Number"
 )
 def get_player_by_squad_number(
     squad_number: int = Path(..., title="The Squad Number of the Player"),
     orm_session: Session = Depends(get_orm_session)
 ):
-    player = player_service.retrieve_player_by_squad_number(orm_session, squad_number)
+    player = player_service.retrieve_by_squad_number(orm_session, squad_number)
+
     if not player:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
     return player
+
+# PUT --------------------------------------------------------------------------
+
+
+@api_router.put(
+    "/players/{player_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    summary="Updates an existing Player"
+)
+def update_player(
+    player_id: int = Path(..., title="The Id of the Player"),
+    player_model: PlayerModel = None,
+    orm_session: Session = Depends(get_orm_session),
+
+):
+    if player_model is None:
+        player_model = Body(...)
+
+    if not player_model:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+    player = player_service.retrieve_by_id(orm_session, player_id)
+
+    if not player:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    player_service.update(orm_session, player_model)
+
+# DELETE -----------------------------------------------------------------------
+
+
+@api_router.delete(
+    "/players/{player_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    summary="Deletes an existing Player"
+)
+def delete_player(
+    player_id: int = Path(..., title="The Id of the Player"),
+    orm_session: Session = Depends(get_orm_session)
+):
+
+    player = player_service.retrieve_by_id(orm_session, player_id)
+
+    if not player:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Player not found")
+
+    player_service.delete(orm_session, player_id)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
-# -------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Test
-# -------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 from main import app
 from fastapi.testclient import TestClient
@@ -8,24 +8,19 @@ import warnings
 # Suppress the DeprecationWarning from httpx
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-
 client = TestClient(app)
+PATH = "/players/"
 
-# -------------------------------------------------------------------------------------------------
-# HTTP GET
-# -------------------------------------------------------------------------------------------------
-
-
-# GET /players
+# GET --------------------------------------------------------------------------
 
 
-def test_when_request_has_no_parameters_then_response_status_code_should_be_200_ok():
-    response = client.get("/players")
+def test_given_get_when_request_has_no_parameters_then_response_status_code_should_be_200_ok():
+    response = client.get(PATH)
     assert response.status_code == 200
 
 
-def test_when_request_has_no_parameters_then_response_body_should_be_collection_of_players():
-    response = client.get("/players")
+def test_given_get_when_request_has_no_parameters_then_response_body_should_be_collection_of_players():
+    response = client.get(PATH)
     players = response.json()
     player_id = 0
     for player in players:
@@ -33,45 +28,39 @@ def test_when_request_has_no_parameters_then_response_body_should_be_collection_
         assert player["id"] == player_id
 
 
-# GET /players/{id}
-
-
-def test_when_request_parameter_does_not_identify_a_player_then_response_status_code_should_be_404_not_found():
+def test_given_get_when_request_parameter_does_not_identify_a_player_then_response_status_code_should_be_404_not_found():
     player_id = 999
-    response = client.get("/players/" + str(player_id))
+    response = client.get(PATH + str(player_id))
     assert response.status_code == 404
 
 
-def test_when_request_parameter_identifies_existing_player_then_response_status_code_should_be_200_ok():
+def test_given_get_when_request_parameter_identifies_existing_player_then_response_status_code_should_be_200_ok():
     player_id = 1
-    response = client.get("/players/" + str(player_id))
+    response = client.get(PATH + str(player_id))
     assert response.status_code == 200
 
 
-def test_when_request_parameter_identifies_existing_player_then_response_body_should_be_matching_player():
+def test_given_get_when_request_parameter_identifies_existing_player_then_response_body_should_be_matching_player():
     player_id = 1
-    response = client.get("/players/" + str(player_id))
+    response = client.get(PATH + str(player_id))
     player = response.json()
     assert player["id"] == player_id
 
 
-# GET /players/squadnumber/{squad_number}
-
-
-def test_when_request_parameter_is_non_existing_squad_number_then_response_status_code_should_be_404_not_found():
+def test_given_get_when_request_parameter_is_non_existing_squad_number_then_response_status_code_should_be_404_not_found():
     squad_number = 999
-    response = client.get("/players/squadnumber/" + str(squad_number))
+    response = client.get(PATH + "squadnumber" + "/" + str(squad_number))
     assert response.status_code == 404
 
 
-def test_when_request_parameter_is_existing_squad_number_then_response_status_code_should_be_200_ok():
+def test_given_get_when_request_parameter_is_existing_squad_number_then_response_status_code_should_be_200_ok():
     squad_number = 10
-    response = client.get("/players/squadnumber/" + str(squad_number))
+    response = client.get(PATH + "squadnumber" + "/" + str(squad_number))
     assert response.status_code == 200
 
 
-def test_when_request_parameter_is_existing_squad_number_then_response_body_should_be_matching_player():
+def test_given_get_when_request_parameter_is_existing_squad_number_then_response_body_should_be_matching_player():
     squad_number = 10
-    response = client.get("/players/squadnumber/" + str(squad_number))
+    response = client.get(PATH + "squadnumber" + "/" + str(squad_number))
     player = response.json()
     assert player["squadNumber"] == squad_number
