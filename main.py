e912bb3ff8e94c19c21251ce999f061e9851465a
Nano Taboada <nanotaboada@users.noreply.github.com>
2024-07-29 12:43:51 -0300

    refactor!: support for async code

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ from routes import player_route
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan_context_manager(_):
+    """
+    Context manager for the FastAPI app lifespan.
+
+    Initializes  FastAPICache with an InMemoryBackend for the duration of the app's lifespan.
+    """
     FastAPICache.init(InMemoryBackend())
     yield
 
-app = FastAPI(lifespan=lifespan,
+app = FastAPI(lifespan=lifespan_context_manager,
               title="python-samples-fastapi-restful",
               description="🧪 Proof of Concept for a RESTful API made with Python 3 and FastAPI",
               version="1.0.0",)
